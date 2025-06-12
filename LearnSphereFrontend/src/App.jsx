5eb0225c66814d4d50dc4d59356ea6d5cc3552f2
Rupam-Mondal <rupammondal564@gmail.com>
2025-06-12 11:03:00 +0530

    Update

--- LearnSphereFrontend/src/App.jsx
+++ LearnSphereFrontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Components/Navbar/Navbar";
 function App(){
   return (
     <>
-      <div className="relative w-full">
+      <div className="relative w-full min-h-screen">
         <Navbar/>
       </div>
       
