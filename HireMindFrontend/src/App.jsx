154ae38333492530c806918c3f1e7282f5ccf78c
Sandipan_seth <sandipanseth2004@gmail.com>
2025-06-12 09:59:23 +0530

    Tailwind

--- HireMindFrontend/src/App.jsx
+++ HireMindFrontend/src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./Components/Navbar/Navbar";
+import "./App.css";
 
 function App(){
   return (
     <>
-      <div className="relative h-screen w-full">
+      <div className="relative h-[200vh] w-full">
         <Navbar/>
       </div>
       
