c07ee87508a54d7b695ad913f2963339909b4fe3
Sandipan_seth <sandipanseth2004@gmail.com>
2025-06-18 10:30:30 +0530

    Border removed

--- LearnSphereFrontend/src/pages/Home.jsx
+++ LearnSphereFrontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className=" ">
       <Navbar />
-      <div className="relative py-10 w-full h-auto flex flex-col justify-center items-center border border-red-500 min-h-screen">
+      <div className="relative py-10 w-full h-auto flex flex-col justify-center items-center min-h-screen">
         <Stylebar />
         <LandingSection />
       </div>
