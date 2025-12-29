bb4c652812a5acc0f06b26459d29237272ba2920
Sandipan Seth <sandipanseth2004@gmail.com>
2025-12-30 00:20:52 +0530

    hero section

--- LearnSphereFrontend/src/pages/Home.jsx
+++ LearnSphereFrontend/src/pages/Home.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import Navbar from '../Components/Navbar/Navbar';
-import Stylebar from '../Components/Stylebar/Stylebar';
-import LandingSection from '../Components/Landing/Landingsection';
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import Stylebar from "../Components/Stylebar/Stylebar";
+import LandingSection from "../Components/Landing/Landingsection";
 
 const Home = () => {
   return (
     <div className=" ">
       <Navbar />
-      <div className="relative py-10 w-full h-auto flex flex-col justify-center items-center min-h-screen">
-        <Stylebar />
-        <LandingSection />
-      </div>
+      <LandingSection />
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
