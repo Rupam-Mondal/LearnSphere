8286c9837c719334ce7745bc8030ae9f70d8bdc7
Sandipan Seth <sandipanseth2004@gmail.com>
2025-12-31 03:22:50 +0530

    CORS

--- LearnSphereBackend/src/app.js
+++ LearnSphereBackend/src/app.js
@@ -10,7 +10,18 @@ import studentRouter from "./routes/studentRoute.js";
 import commentRouter from "./routes/commentRoute.js";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local frontend
+      "https://learn-sphere-7kcz.vercel.app", // deployed frontend
+    ],
+    credentials: true, // VERY IMPORTANT
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,16 +31,13 @@ app.get("/", (req, res) => {
   res.send("Welcome to the LearnSphere Backend!");
 });
 
+
+
 app.use("/api/ai", airouter);
 app.use("/api/auth", authrouter);
-
-
 app.use("/api/teacher", teacherRouter);
-
 app.use("/api/admin", adminRouter);
-
 app.use("/api/student", studentRouter);
-
 app.use("/api/comment", commentRouter);
 
 export default app;
