2e623ab9ed3f1636d2075aa32d1f4064efc9dd1a
Rupam-Mondal <rupammondal564@gmail.com>
2026-05-02 15:57:02 +0530

    Why choose us section

--- LearnSphereFrontend/src/pages/Home.jsx
+++ LearnSphereFrontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import LandingSection from "../Components/Landing/Landingsection";
 import Courses from "../Components/Landing/Courses";
 import Footer from "../Components/Landing/Footer";
+import WhychooseUs from "../Components/Landing/WhychooseUs";
 
 const Home = () => {
   return (
     <div className=" ">
       <LandingSection />
+      <WhychooseUs/>
       <Courses />
       <Footer />
     </div>
