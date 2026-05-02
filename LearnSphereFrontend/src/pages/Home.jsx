118a7d306760a0ca208f212294eeaeb588ddde56
Rupam-Mondal <rupammondal564@gmail.com>
2026-05-02 16:02:57 +0530

    Update

--- LearnSphereFrontend/src/pages/Home.jsx
+++ LearnSphereFrontend/src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import LandingSection from "../Components/Landing/Landingsection";
 import Courses from "../Components/Landing/Courses";
 import Footer from "../Components/Landing/Footer";
-import WhychooseUs from "../Components/Landing/WhychooseUs";
+
 
 const Home = () => {
   return (
     <div className=" ">
       <LandingSection />
-      <WhychooseUs/>
       <Courses />
       <Footer />
     </div>
